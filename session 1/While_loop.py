99e32c671cbaab8f36d93c317accaa58cc4d1a0d
asharathore <asha8302268377@gmail.com>
2021-01-24 17:27:52 +0530

    added session files

---
+++ session 1/While_loop.py
@@ -0,0 +1,5 @@
+ 1ampturned on = "true"
+while 1ampturned oon  == "true":
+    1ampturned on = input("please continue reading your book. Is the 1amp turned on ?(say true if on):")
+
+print("1amp turned off! go for sleep")
---
+++ session2/if - elif- else.py
@@ -0,0 +1,26 @@
+num = 50
+
+if num == 0:
+    print("zero")
+
+elif num == 1:
+    print("one")
+
+elif num == 2:
+    print("two")
+
+elif num == 3:
+    print("three")
+
+elif num == 4:
+    print("four")
+
+elif num == 5:
+    print("five")
+
+elif num == 6:
+    print("six")
+
+else:
+    print("invalid number")
+    
